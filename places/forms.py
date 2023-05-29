539823897c4a570dc021c876bdff739c59da8128
Konstantin <kostikvlz@mail.ru>
2023-05-29 08:08:47 +0300

    added map widget

--- places/forms.py
+++ places/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
 from .models import Place
+from django.contrib.gis import forms
 
 
 class PlaceForm(ModelForm):
     class Meta:
         model = Place
         fields = ('location', 'name', 'comment')
+        widgets = {
+            'location': forms.OSMWidget(attrs={
+                'default_lat': 43.6028,
+                'default_lon': 39.7342
+            })
+        }
